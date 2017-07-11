8c8845c5492d3db94a64301290f2e6bdb5c22732
Zeroxx <franciscopa91@gmail.com>
2017-07-11 15:36:31 +0200

    Completed

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 /**
  * Created by fran on 9/7/17.
  */
-
+import _ from 'lodash';
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
@@ -40,6 +40,10 @@ class App extends Component {
     }
 
     render(){
+        const videoSearch = _.debounce((term) => {
+            this.videoSearch(term)
+        },300)
+
         return (
             <div>
                 <SearchBar onSearchTermChange={term => this.videoSearch(term)}/>
